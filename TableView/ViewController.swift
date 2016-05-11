34e305f61c287b9f37b94791dcea5361125d6851
Melvin Musehani <melvinmusehani@melvins-macbook-pro.local>
2016-05-11 15:18:41 +0200

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Melvin Musehani on 2016/05/11.
+//  Copyright © 2016 Appchemy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
